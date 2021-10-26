de7fce4da9b117a91546a505b6806308b9d1b3e4
GustavoSierra <gsierra.business@gmail.com>
2021-10-27 01:42:04 +0200

    creacion de members para almacenamiento de datos de clientes, primero clientes añadidos

---
+++ collection/members.js
@@ -0,0 +1,34 @@
+const members = [
+    {
+        id: 1,
+        name: 'Gustavo',
+        email: 'gustavo@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Pablo',
+        email: 'pablo@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Brajin',
+        email: 'brahin@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Raul',
+        email: 'raul@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 5,
+        name: 'Gonzalo',
+        email: 'gonzalo@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members 
